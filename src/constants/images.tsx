97d0cd17fd507605880b0f6faa52309c24c6e214
Peter_Atef <ando9910@gmail.com>
2021-08-22 18:11:29 +0200

    Finished baic themeing and navBar links

---
+++ src/constants/images.tsx
@@ -0,0 +1,18 @@
+import { ReactComponent as LOGO } from '../View/assets/site-logo.svg'
+
+/** 
+*  Usage: render() { <img src = {APP_IMAGES.IMAGENAME}/> }
+*/
+const APP_IMAGES = {
+
+}
+
+
+/** 
+* Usage: render() {<APP_SVG.ICONNAME/> }
+*/
+const APP_SVG = {
+    LOGO,
+}
+
+export { APP_IMAGES, APP_SVG }
