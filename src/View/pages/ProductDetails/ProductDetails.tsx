8f19e280fe96be365635eeadc327fba7e6528d81
Peter_Atef <ando9910@gmail.com>
2021-09-04 20:33:23 +0200

    Created some functions on the product details page

--- src/View/pages/ProductDetails/ProductDetails.tsx
+++ src/View/pages/ProductDetails/ProductDetails.tsx
@@ -10,14 +10,28 @@ type Props = {
 }
 class ProductDetails extends Component<Props> {
     render() {
-        return (
+        let currentProduct: ProductProps;
+        let images: string[] = [];
+        let title: string = "";
+        if (!this.props.loading) {
+            try {
+                currentProduct = this.props.products[this.props.selectedProduct]
+                for (let i = 0; i < currentProduct.gallery.length; i++) {
+                    images.push(currentProduct.gallery[i])
+                }
+                title = currentProduct.name;
+            } catch (e) {
+                console.log(e)
+            }
+        }
+        return this.props.loading ? (
             <div>
-                <p>
-                    {this.props.loading ? "Loading..." : this.props.selectedProduct}
-                    {/* {this.props.product} */}
-                </p>
+                LoadingPage
             </div>
-        )
+        ) :
+            <div>
+                {title}
+            </div>
     }
 }
 
